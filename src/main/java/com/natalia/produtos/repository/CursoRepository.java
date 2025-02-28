83ee79ed6e967c9b754e5854bcd4aa7f07196f00
natalia <nataalia.missio@gmail.com>
2025-02-28 15:50:40 -0300

    preparando camadada de dados e negocio

--- src/main/java/com/natalia/produtos/repository/CursoRepository.java
+++ src/main/java/com/natalia/produtos/repository/CursoRepository.java
@@ -1,5 +1,8 @@
 package com.natalia.produtos.repository;
 
-public interface CursoRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.natalia.produtos.entity.Curso;
+
+public interface CursoRepository extends JpaRepository<Curso,Long>{
 }
--- src/main/java/com/natalia/produtos/service/CursoService.java
+++ src/main/java/com/natalia/produtos/service/CursoService.java
@@ -1,5 +1,37 @@
 package com.natalia.produtos.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.natalia.produtos.entity.Curso;
+import com.natalia.produtos.repository.CursoRepository;
+
+@Service
 public class CursoService {
+  private final CursoRepository cursoRepository;
+
+  @Autowired
+  public CursoService(CursoRepository cursoRepository) {
+    this.cursoRepository = cursoRepository;
+  }
+
+  public Curso salvarCurso(Curso curso) {
+    return cursoRepository.save(curso);
+  }
+
+  public Optional<Curso> buscarCursoPorId(Long id) {
+    return cursoRepository.findById(id);
+  }
+
+  public List<Curso> buscarTodosCursos() {
+    return cursoRepository.findAll();
+  }
+
+  public void excluirCurso(Long id) {
+    cursoRepository.deleteById(id);
+  }
 
 }
